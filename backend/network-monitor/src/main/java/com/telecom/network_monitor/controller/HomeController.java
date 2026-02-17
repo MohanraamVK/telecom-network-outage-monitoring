f3fb0e0b97789a959dda5b8b7b596b9583017c8c
Mohan <vkmr2001@gmail.com>
2026-02-17 22:21:26 +0100

    Created base layered architecture structure

--- backend/network-monitor/src/main/java/com/telecom/network_monitor/controller/HomeController.java
+++ backend/network-monitor/src/main/java/com/telecom/network_monitor/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.telecom.network_monitor;
+package com.telecom.network_monitor.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
